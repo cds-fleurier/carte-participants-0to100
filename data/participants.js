ae9499e754defa4091459827abbe6f46454cfa16
cds-fleurier <fleurier@digbysmith.com>
2026-07-21 07:05:44 +0200

    Ajout Marina (0 to 40) + Wadie (0 to 100)

--- data/participants.js
+++ data/participants.js
@@ -27,4 +27,6 @@ window.PARTICIPANTS = [
   { id: "myriam",   name: "Myriam",   city: "Aiglun",    lat: 44.0509, lng: 6.1442, birthDay: 22, birthMonth: 3, group: "100", photo: "assets/photos/myriam.jpeg" },
   { id: "alice",    name: "Alice",    city: "Ignaux",    lat: 42.7319, lng: 1.8400, birthDay: 1,  birthMonth: 7, group: "100", photo: "assets/photos/alice.jpeg" },
   { id: "jb",       name: "JB",       city: "Saint-Étienne", lat: 45.4401, lng: 4.3873, birthDay: 25, birthMonth: 8, group: "100", photo: "assets/photos/JB.jpeg" },
+  { id: "marina",   name: "Marina",   city: "Malvalette", lat: 45.3545, lng: 4.1584, birthDay: 11, birthMonth: 8, group: "40",  photo: "assets/photos/Marina.jpeg" },
+  { id: "wadie",    name: "Wadie",    city: "Montagny",   lat: 45.6284, lng: 4.7471, birthDay: 22, birthMonth: 3, group: "100", photo: "assets/photos/Wadie.jpeg" },
 ];
