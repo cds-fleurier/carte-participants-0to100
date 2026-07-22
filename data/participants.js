e8e9de5c6b935484d565eba5daa56d511f095d98
cds-fleurier <fleurier@digbysmith.com>
2026-07-22 18:19:03 +0200

    Ajout Virginie (0 to 100, Saint-Romain-les-Atheux)

--- data/participants.js
+++ data/participants.js
@@ -56,4 +56,5 @@ window.PARTICIPANTS = [
   { id: "solene",   name: "Solène",   city: "Nantes",    lat: 47.2186, lng: -1.5541, birthDay: 2, birthMonth: 3, group: "100", photo: "assets/photos/Solene.jpeg" },
   { id: "morgane",  name: "Morgane",  city: "Montrond-les-Bains", lat: 45.6433, lng: 4.2299, birthDay: 16, birthMonth: 9, group: "100", photo: "assets/photos/Morgane.jpeg" },
   { id: "jerome",   name: "Jerome",   city: "Apinac",    lat: 45.3803, lng: 3.9962, birthDay: null, birthMonth: null, group: "100", photo: "assets/photos/Jerome.jpeg" }, // date à confirmer (« 31 avril » invalide)
+  { id: "virginie", name: "Virginie", city: "Saint-Romain-les-Atheux", lat: 45.3561, lng: 4.3769, birthDay: 17, birthMonth: 4, group: "100", photo: "assets/photos/Virginie.jpeg" },
 ];
